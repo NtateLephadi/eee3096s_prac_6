827146e38a68687e7cbc442028795449ded68221
Student <Student@SCI000A068.wf.uct.ac.za>
2018-10-17 10:45:06 +0200

    add comments to password

--- password.py
+++ password.py
@@ -2,45 +2,35 @@ from character import character
 import sys
 
 class password:
-    
+    """ A single password """
+
+    """ __init__ """
     def __init__(self, code):
         self.code=[]
-        
+
+    """ set code """
     def set_code(self, code):
         self.code=code
-        
+
+    """ return current code """
     def get_code(self, code):
         return self.code
-    
+
+    """ add a character to code """
     def add_character(self, character):
         self.code.append(character)
-        
+
+    """ validate code in secure mode """
     def validate_secure(self, code):
         return str(self.to_string()) == str(code.to_string())
-    
+
     def validate_unsecure(self, other_password):
-        return sorted(self.code)==sorted(other_password.code)          
-        
+        """ validate code in unsecure mode """
+        return sorted(self.code)==sorted(other_password.code)
+
+    """ return code as string """
     def to_string(self):
         s=""
         for i in self.code:
             s = i.to_string() + " " + s
         return s
-        
-c1=character('A', 2) 
-c2=character('C', 3)
-
-c3=character('A', 2) 
-c4=character('C', 3)
-
-p1=password([])
-p2=password([])
-
-p2.add_character(c1)
-p2.add_character(c2)
-p1.add_character(c3)
-p1.add_character(c4)
-
-
-print(p1.validate_secure(p2))
-print(p1.validate_unsecure(p2))
